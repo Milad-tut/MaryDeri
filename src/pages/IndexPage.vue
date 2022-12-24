f77a2798e72244b52e808b2d6d65df830726bb56
milad4444 <miladsarli4444@gmail.com>
2022-12-24 17:38:01 +0330

    ilhan branhc created

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,7 @@
 <template>
   <q-page>
-    <div class="row full-width justify-center q-my-md">
-      <q-btn no-caps flat class="bg-primary text-white" label=" Hi Dear Milad " />
-    </div>
     <h1 class="text-center">
-      Ilhan sarli
-      milad sarli
+      Ilhan Branch
     </h1>
   </q-page>
 </template>
