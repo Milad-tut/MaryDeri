d6a0022f9830a3b9714c7580a97d8b519541a0d7
milad4444 <miladsarli4444@gmail.com>
2022-12-24 17:21:04 +0330

    a button added to index page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,9 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>
+  <q-page>
+    <div class="row full-width justify-center q-my-md">
+      <q-btn no-caps flat class="bg-primary text-white" label=" Hi Dear Milad " />
+    </div>
+    <h1 class="text-center">
       Ilhan sarli
       milad sarli
     </h1>
