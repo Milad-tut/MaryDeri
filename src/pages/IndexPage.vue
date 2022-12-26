bb6651239879624709688a9a40afd7d80ef9bc92
milad4444 <miladsarli4444@gmail.com>
2022-12-26 16:55:55 +0330

    last changes

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -8,12 +8,25 @@
     <q-chip icon="bookmark">Bookmark</q-chip>
     <q-chip icon="alarm" label="Set alarm" />
     <q-chip class="glossy" icon="directions">Get directions</q-chip>
+    <q-btn-toggle v-model="model" toggle-color="primary" :options="[
+  { label: 'One', value: 'one' },
+  { label: 'Two', value: 'two' },
+  { label: 'Three', value: 'three' }
+]" />
+    <q-checkbox v-model="right" label="Label on Right" />
+    <q-img src="https://placeimg.com/500/300/nature" :ratio="16 / 9" />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { ref, defineComponent } from 'vue'
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  setup () {
+    return {
+      model: ref(null),
+      right: ref(false),
+    }
+  }
 })
 </script>
