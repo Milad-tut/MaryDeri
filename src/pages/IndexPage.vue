c0d4f3c4ce395d11cd695d353286317d81afa155
ilhanesk <eilhan.eskandarnezhad@gmail.com>
2023-01-07 13:49:36 +0100

    changes

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,28 +2,77 @@
 <q-page>
 <div class="text-h1 text-center">maryderi</div>
 <div class="row full-width justify-center">
-   <q-img src="images/leather-4.jpg" spinner-color="white" style="height: 400px; max-width: 1400px;" class="q-mt-xl" /> 
+   <q-img src="images/leather-4.jpg" spinner-color="white" style="height: 400px; max-width: 1400px;  border:2px solid #fff;
+  background: url(img/tiger.png) no-repeat;
+  -moz-box-shadow: 10px 10px 5px #ccc;
+  -webkit-box-shadow: 10px 10px 5px #ccc;
+  box-shadow: 10px 10px 5px #ccc;
+  -moz-border-radius:25px;
+  -webkit-border-radius:25px;
+  border-radius:25px;" class="q-mt-xl" /> 
 </div>
 <div class="row full-width justify-center q-my-md">
    <div class="col-10 row justify-center q-col-gutter-lg">
       <div class="col-4">
         <q-img src="https://marvel-b1-cdn.bc0a.com/f00000000084691/www.krsaddleshop.com/img/2022-10__01/KRSS_MAIN_SLIDER_TEMPLATE_03b.jpg"
-        spinner-color="white" style="height: 240px;" @click="$router.push('/products/women wallet')" />
+        spinner-color="white" style="height: 240px;  border:2px solid #fff;
+  background: url(img/tiger.png) no-repeat;
+  -moz-box-shadow: 10px 10px 5px #ccc;
+  -webkit-box-shadow: 10px 10px 5px #ccc;
+  box-shadow: 10px 10px 5px #ccc;
+  -moz-border-radius:25px;
+  -webkit-border-radius:25px;
+  border-radius:25px;" @click="$router.push('/products/women wallet')" />
       </div>
       <div class="col-4"><q-img src="https://marvel-b1-cdn.bc0a.com/f00000000084691/www.krsaddleshop.com/img/landing_page_assets/cms-page__office-spotlight_bg.jpg"
-         spinner-color="white" style="height: 240px;" @click="$router.push('/products/purse')"/>
+         spinner-color="white" style="height: 240px;  border:2px solid #fff;
+  background: url(img/tiger.png) no-repeat;
+  -moz-box-shadow: 10px 10px 5px #ccc;
+  -webkit-box-shadow: 10px 10px 5px #ccc;
+  box-shadow: 10px 10px 5px #ccc;
+  -moz-border-radius:25px;
+  -webkit-border-radius:25px;
+  border-radius:25px;" @click="$router.push('/products/purse')"/>
       </div>
       <div class="col-4"><q-img src="https://trulyexperiences.com/blog/wp-content/uploads/2020/03/brown-binoculars-camera-and-bag-kit-2282479-scaled-e1584029733887.jpg"
-         spinner-color="white" style="height: 240px;" @click="$router.push('/products/shoes')"/>
+         spinner-color="white" style="height: 240px;  border:2px solid #fff;
+  background: url(img/tiger.png) no-repeat;
+  -moz-box-shadow: 10px 10px 5px #ccc;
+  -webkit-box-shadow: 10px 10px 5px #ccc;
+  box-shadow: 10px 10px 5px #ccc;
+  -moz-border-radius:25px;
+  -webkit-border-radius:25px;
+  border-radius:25px;" @click="$router.push('/products/shoes')"/>
       </div>
       <div class="col-4"><q-img src="https://assets.aspinaloflondon.com/images/block-feature-2021-large/1486204-106718-image-3-storage.jpg"
-         spinner-color="white" style="height: 240px;" @click="$router.push('/products/belt')"/>
+         spinner-color="white" style="height: 240px;  border:2px solid #fff;
+  background: url(img/tiger.png) no-repeat;
+  -moz-box-shadow: 10px 10px 5px #ccc;
+  -webkit-box-shadow: 10px 10px 5px #ccc;
+  box-shadow: 10px 10px 5px #ccc;
+  -moz-border-radius:25px;
+  -webkit-border-radius:25px;
+  border-radius:25px;" @click="$router.push('/products/belt')"/>
       </div>
       <div class="col-4"><q-img src="https://www.fossil.com/on/demandware.static/-/Library-Sites-FossilSharedLibrary/default/dw96fe84ef/2022/HO22/set_12262022_global/1226_SemiAnnual_Sale_Global_Grid_2_hover.jpg"
-         spinner-color="white" style="height: 240px;" @click="$router.push('/products/wristlet')"/>
+         spinner-color="white" style="height: 240px;  border:2px solid #fff;
+  background: url(img/tiger.png) no-repeat;
+  -moz-box-shadow: 10px 10px 5px #ccc;
+  -webkit-box-shadow: 10px 10px 5px #ccc;
+  box-shadow: 10px 10px 5px #ccc;
+  -moz-border-radius:25px;
+  -webkit-border-radius:25px;
+  border-radius:25px;" @click="$router.push('/products/wristlet')"/>
       </div>
       <div class="col-4"><q-img src="https://media-cldnry.s-nbcnews.com/image/upload/newscms/2022_21/1874437/31-03-2017-000009012-son-2048x-628e6013ea8ad.jpg"
-         spinner-color="white" style="height: 240px;" @click="$router.push('/products/wristband')"/>
+         spinner-color="white" style="height: 240px;  border:2px solid #fff;
+  background: url(img/tiger.png) no-repeat;
+  -moz-box-shadow: 10px 10px 5px #ccc;
+  -webkit-box-shadow: 10px 10px 5px #ccc;
+  box-shadow: 10px 10px 5px #ccc;
+  -moz-border-radius:25px;
+  -webkit-border-radius:25px;
+  border-radius:25px;" @click="$router.push('/products/wristband')"/>
       </div>
    </div>
 </div>
